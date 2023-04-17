105d600d98e176e6c464de1152d8d2c46bb0d8a8
couvev <118241471+couvev@users.noreply.github.com>
2023-04-17 08:40:11 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,6 +1,18 @@
 package q1
 
+import "fmt"
+
+func main(){
+	
+	
+	
+}
+
 func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
 	// Implemente sua solução aqui
+	
+	
+	
+	
 	return 0, nil
 }
