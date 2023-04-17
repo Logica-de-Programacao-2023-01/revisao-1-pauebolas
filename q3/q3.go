3cf71f70e1bca4b1a5a7d142c25de7a8b7543a32
couvev <118241471+couvev@users.noreply.github.com>
2023-04-17 14:54:06 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -1,6 +1,47 @@
-package q3
+package main
+
+import "fmt"
+
+func main(){
+    
+    maior, menor, media, err := FindMinMaxAverage([]int{1})
+    fmt.Printf("%d %d %f %s", maior, menor, media, err)
+}
 
 func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
-	// Implemente sua solução aqui
-	return 0, 0, 0, nil
+    
+    maior := 0
+    menor := 999999
+    media := 0.
+    x := 0
+    total := 0
+    
+    if len(numbers) == 0 {
+        
+        return 0, 0, 0, fmt.Errorf("Lista vazia!")
+        
+    }
+    
+    for x = 0; x < len(numbers); x++{
+        
+        total += numbers[x]
+        
+        if numbers[x] > maior{
+            
+            maior = numbers[x]
+            
+        } 
+        if numbers[x] < menor{
+            
+            menor = numbers[x]
+            
+        }
+        
+    }
+
+    total = total - (maior+menor)
+    
+    media = float64(total) / float64(x)
+    
+    return maior, menor, media, nil
 }
